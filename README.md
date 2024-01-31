2c3551665d52c7c201d45c6158f0e77be80a2d3b
petemaynard <xerxespete@gmail.com>
2024-01-30 22:57:30 -0600

    working but needs some cleanup

---
+++ README.md
@@ -0,0 +1,26 @@
+![Creative Commons Badge](https://img.shields.io/badge/License-CC_BY-EF9421.svg?style=for-the-badge)
+# 1
+## Table of Contents 
+ - [Description](#description) 
+ - [Installation](#installation) 
+ - [Usage](#usage) 
+ - [How to Contribute](#how-to-contribute) 
+ - [Tests](#tests) 
+ - [License](#license) 
+ - [Questions](#questions)
+## Description 
+ 2
+## Installation 
+ 3
+## Usage 
+ 4
+## How to Contribute 
+ 5
+## Tests 
+ 6
+## License.  
+This software is available under the Creative Commons license.
+## Questions 
+My GitHub account can be found at https://github.com/8 
+
+You can reach me at 9
--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const questions = require('./questions');
 
 let markupA1, markupA2, markupA3, markupA4, markupA5, markupA6, markupA7, markupA8, markupA9;
 let badgeURL;
-var q8Github, q9Email
-var finalMarkup;
+let q8Github, q9Email
+let finalMarkup;
+const nl = "\n";
+let TOC;
 
 inquirer.prompt(questions)
    .then((answers) => {
@@ -20,80 +22,97 @@ inquirer.prompt(questions)
       q8Github = answers.q8github;
       q9Email = answers.q9email
    })
+
    // Now compile all the answers into the README file
-   .then(function (){
+   .then(function () {
       makeReadme(q8Github, q9Email);
    });
 
 
-   //Project Title
-function q1(answer1) {
 
+//Project Title
+function q1(answer1) {
    markupA1 = `# ${answer1}`;
-     console.log("markup1 is: ", markupA1); 
+   // fs.appendFile('README.md', `${markupA1}\n`, (err) =>
+   //    err ? console.error(err) : console.log('A1 Commit logged!'));
+   //   console.log("markup1 is: ", markupA1); 
 }
 
 // Project Description
 function q2(answer2) {
-   markupA2 = "## Description \n ", answer2;
-   console.log("markup2 is: ", markupA2); 
+   markupA2 = `## Description \n ${answer2}`;
+   // fs.appendFile('README.md', `${markupA2}\n`, (err) =>
+   //    err ? console.error(err) : console.log('A2 Commit logged!'));
+   // console.log("markup2 is: ", markupA2); 
 }
 
 // Installation Instructions
 function q3(answer3) {
-   markupA3 = "## Installation \n", answer3;
-   console.log("markup3 is: ", markupA3); 
+   markupA3 = `## Installation \n ${answer3}`;
+   // fs.appendFile('README.md', `${markupA3}\n`, (err) =>
+   //    err ? console.error(err) : console.log('A3 Commit logged!'));
+   // console.log("markup3 is: ", markupA3); 
 }
 
 //Usage Information
 function q4(answer4) {
-   markupA4 = "## Usage \n", answer4;
-      console.log("markupA4 is: ", markupA4);
+   markupA4 = `## Usage \n ${answer4}`;
+   // fs.appendFile('README.md', `${markupA4}\n`, (err) =>
+   //    err ? console.error(err) : console.log('A4 Commit logged!'));
+   // console.log("markupA4 is: ", markupA4);
 }
 
 // How to contribute
 function q5(answer5) {
-   markupA5 = "## How to Contribute \n", answer5;
-   console.log("markupA5 is: ", markupA5);
-   }
+   markupA5 = `## How to Contribute \n ${answer5}`;
+   // fs.appendFile('README.md', `${markupA5}\n`, (err) =>
+   //    err ? console.error(err) : console.log('A5 Commit logged!'));
+   // console.log("markupA5 is: ", markupA5);
+}
 
 
 // Testing instructions
 function q6(answer6) {
-   markupA6 = "## Tests \n", answer6;
-   console.log("markupA6 is: ", markupA6);
+   markupA6 = `## Tests \n ${answer6}`;
+
+   // console.log("markupA6 is: ", markupA6);
 }
 
 // Which license
 function q7(answer7) {
-   console.log("The answer to question number 7 was: ", answer7);
-   if (answer7 == 1){
-      badgeURL = "https://img.shields.io/badge/License-MIT-yellow.svg?style=for-the-badge";
+
+   if (answer7 == 1) {
+      badgeURL = "![MIT Badge](https://img.shields.io/badge/License-MIT-yellow.svg?style=for-the-badge)";
       markupA7 = ("## License.  \nThis software is available under the MIT license.")
    } else if (answer7 == 2) {
-      badgeURL = "https://img.shields.io/badge/License-CC_BY-EF9421.svg?style=for-the-badge"
+      badgeURL = "![Creative Commons Badge](https://img.shields.io/badge/License-CC_BY-EF9421.svg?style=for-the-badge)"
       markupA7 = ("## License.  \nThis software is available under the Creative Commons license.")
    } else {
-      badgeURL = "https://img.shields.io/badge/License-GPL_3-blue.svg?style=for-the-badge"
+      badgeURL = "![GNU Badge](https://img.shields.io/badge/License-GPL_3-blue.svg?style=for-the-badge)"
       markupA7 = ("## License.  \nThis software is available under the GNU GPL v3 license.")
    }
-   console.log("markupA7 is: ", markupA7);
 }
 
 
 function makeReadme(github, email) {
-// This is where we compile all the data into a single file.
-// Or we have a single function called 'append' to append each answer to the outfile
-console.log("Where I join the data together.")
+   // This is where we compile all the data into a single file.
+   TOC=`## Table of Contents \n - [Description](#description) \n - [Installation](#installation) \n - [Usage](#usage) \n - [How to Contribute](#how-to-contribute) \n - [Tests](#tests) \n - [License](#license) \n - [Questions](#questions)`
 
-markupA8 =(`## Questions \nMy GitHub accout can be found at https://github.com/${github}`);
-markupA9 =(`## You can reach me at ${email}`)
-finalMarkup=[markupA1, markupA2, markupA3, markupA4, markupA5, markupA6, markupA7, markupA8, markupA9]
-console.log("The final markup is this:" + finalMarkup);
+   markupA8 = (`## Questions \nMy GitHub account can be found at https://github.com/${github} \n`);
+   markupA9 = (`You can reach me at ${email}`)
+   finalMarkdown = [badgeURL, markupA1, TOC, markupA2, markupA3, markupA4, markupA5, markupA6, markupA7, markupA8, markupA9]
+ 
+   const pObj = finalMarkdown.join(nl);
 
-finalMarkup="";
-for (var i = 0; i < finalMarkup.length; i++){
-// Do something here   
-
-   }
+   fs.unlink('README.md', (err) => {
+      if (err) {
+         console.log("Error deleting file");
+      } else {
+         console.log("README.md has been deleted");
+         fs.writeFile('README.md', pObj, (err) =>
+            err ? console.error(err) : console.log('Success!')
+         );
+      }
+   })
 }
+
